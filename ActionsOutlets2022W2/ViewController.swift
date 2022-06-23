ff7f4c58186f79d2cdf16388210e6a64db13aa53
Taiwo Oyeleye <taiwooyeleye1@gmail.com>
2022-06-23 11:47:36 -0500

    This is Taiye.

--- ActionsOutlets2022W2/ViewController.swift
+++ ActionsOutlets2022W2/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var Label1: UILabel!
+    
+    @IBOutlet var TextField2: UITextField!
+    @IBOutlet var TextField1: UITextField!
+    @IBAction func ButtonOne(_ sender: Any) {
+        TextField3.text = "I have siblings"
+        TextField1.text = "I'm Nigerian"
+        TextField2.text = "I love food"
+    }
+    @IBOutlet var TextField3: UITextField!
 }
-
